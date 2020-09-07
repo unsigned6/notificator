efb3b7640e8e6a559c92748f83bd146d9c57a447
Vladimir Scherbina <vns.scherbina@gmail.com>
2020-09-07 18:47:39 +0300

    Add support for the Redis

---
+++ tests/Notificator.Redis.test.js
@@ -0,0 +1,48 @@
+const { redisPubsub } = require("../etc/config");
+const Notificator = require("../lib/Notificator");
+const notificator = require("../lib/notificatorSingletonRedis");
+const PubSub = require("../lib/PubSub");
+const RedisDriver = require("../lib/drivers/Redis");
+
+describe("Notificator with Redis driver tests", () => {
+  // test("test placeholder", async () => {});
+  test("positive: can notify/receive messages", async () => {
+    await notificator.init();
+    setTimeout(() => {
+      notificator.notify({ test: 1 });
+    }, 100);
+    const result = await new Promise((res) =>
+      notificator.receive((msg) => res(msg))
+    );
+    expect(result.test).toBe(1);
+  });
+  // test("positive: can notify/receive messages between different nodes", async () => {
+  //   const notificatorOne = createNotificator();
+  //   const notificatorTwo = createNotificator();
+  //   await notificatorOne.init();
+  //   await notificatorTwo.init();
+  //   setTimeout(() => {
+  //     notificatorOne.notify({ test: 1 });
+  //   }, 100);
+  //   const result = await new Promise((res) =>
+  //     notificatorTwo.receive((msg) => res(msg))
+  //   );
+  //   expect(result.test).toBe(1);
+  // });
+});
+
+function createNotificator() {
+  const rabbitDriver = new RedisDriver({
+    port: redisPubsub.port,
+    host: redisPubsub.host,
+    database: redisPubsub.database,
+  });
+
+  const notificator = new Notificator({
+    pubsub: new PubSub({
+      driver: rabbitDriver,
+    }),
+  });
+
+  return notificator;
+}
